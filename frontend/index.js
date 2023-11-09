b6589fcf1aeff5b4d3574252bfced6f139ff1eb5
Jorge Lucas <jorgelucasea@gmail.com>
2023-11-09 19:44:56 -0300

    Mudanças para funcionar estação

--- frontend/index.js
+++ frontend/index.js
@@ -1,14 +1,21 @@
 const ridesAPI = 'http://localhost:8000/rides'; // Endpoint da API para manipular os passeios
-
+const stationAPI = 'http://localhost:8000/stations'
 // Função ao carregar a página chama o listRides, que carrega a list na tabela.
 document.addEventListener("DOMContentLoaded", function() {
     listRides()
+    listStations()
     document.getElementById('addRideButton').addEventListener('click', showAddRideForm);
+    document.getElementById('addStationButton').addEventListener('click', showAddStationForm);
 })
 // Pego o botão para adicionar um evento click que quando chamado edit algum ride.
 const btnEdit = document.getElementById("saveRideButton")
 btnEdit.addEventListener("click", function() {
         editRide()
+})
+
+const btn = document.getElementById("saveStationButton")
+btnEdit.addEventListener("click", function() {
+        listStations()
 })
 
 // Função para listar passeios de bicicleta
@@ -188,4 +195,82 @@ function editRide() {
             listRides(); // Atualiza a lista de passeios
         })
         .catch(error => console.error('Erro ao editar passeio de bicicleta:', error));
+}
+
+
+function listStations() {
+    fetch(stationAPI)
+        .then(response => response.json())
+        .then(data => {
+            // Mostra os passeios no subtitulo "passeios de bicicletas" no HTML
+            const rideTable = document.getElementById('stationTable');
+            const tbody = rideTable.querySelector('tbody');
+            tbody.innerHTML = '';
+
+            data.forEach(station => {
+                // Create a new row for each ride
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                    <td>${station.station}</td>
+                    <td>${station.station_number}</td>
+                    <td>${station.lat}</td>
+                    <td>${station.lon}</td>
+                    <td><button class="btn btn-primary edit-button1" data-id="${station.rowid}">Edit</button></td>
+                `;
+                tbody.appendChild(row);
+            });
+
+            const editButtons = document.querySelectorAll('.edit-button1');
+            editButtons.forEach(button => {
+                button.addEventListener('click', () => showForm());
+            });
+
+            
+        })
+        .catch(error => console.error('Erro ao obter passeios de bicicleta:', error));
+}
+
+function showForm() {
+    const rideForm = document.getElementById('stationForm');
+    rideForm.style.display = 'block';
+}
+
+function showAddStationForm() {
+    const rideForm = document.getElementById('stationForm');
+    rideForm.style.display = 'block';
+
+    const btn = document.getElementById("addStationButton")
+    btn.style.display = "none"
+}
+
+function addStation() {
+    
+    // Coletar dados do formulário
+    const station = document.getElementById('station').value;
+    const stationNumber = document.getElementById('station_number').value;
+    const lat = document.getElementById('lat').value;
+    const long = document.getElementById('long').value;
+    //pensei em adicionar opções de estação pra ser selecionada, mas acho que não tem necessidade
+
+    // Enviar dados ao backend
+    fetch(stationAPI, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(
+            {
+                station: station,
+                station_number: stationNumber,
+                lat: lat,
+                lon: long,
+            }
+        )
+    })
+        .then(response => response.json())
+        .then(data => {
+            // Lógica para lidar com a resposta da adição do passeio
+            listStations(); // Atualiza a lista de passeios
+        })
+        .catch(error => console.error('Erro ao adicionar passeio de bicicleta:', error));
 }
